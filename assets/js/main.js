f4c16eb03083e21547cdb844fe1500051a0da707
AliMoradpour <Mr.mrdp@yahoo.com>
2022-08-29 00:12:41 -0700

    complete and responsive the page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,35 @@
-const navMenu = document.getElementById('nav-menu'),
-    navToggle = document.getElementById('nav-toggle'),
-    navClose = document.getElementById('nav-close');
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
 
-// Menu show 
-if(navToggle){
-    navToggle.addEventListener('click',()=>{
-        navMenu.classList.add('show-menu');
-    });
+// Menu show
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
 }
-// Menu Hidden 
-if(navClose){
-    navClose.addEventListener('click',()=>{
-        navMenu.classList.remove('show-menu');
-    });
+// Menu Hidden
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
 }
 
-// Remove nav Mobile 
-const navLink = document.querySelectorAll('.nav-link');
+// Remove nav Mobile
+const navLink = document.querySelectorAll(".nav-link");
 
-function linkAction(){
-    const navMenu = document.getElementById('nav-menu');
-    navMenu.classList.remove('show-menu');
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show-menu");
 }
-navLink.forEach(n=>n.addEventListener('click',linkAction)); 
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+// scrollreveal
+const sr = ScrollReveal({
+  distance: "90px",
+  duration: 3000,
+});
+
+sr.reveal(".home-data", { origin: "top", delay: 400 });
+sr.reveal(".home-img", { origin: "bottom", delay: 600 });
+sr.reveal(".home-footer", { origin: "bottom", delay: 800 });
